c1871fcae8dc3e45c3a4484d6cddf0ab53c2b03b
Stefan <stefanseifeddine@gmail.com>
2023-04-14 00:36:46 +0100

    Initialise currentScore and highScore states

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -2,10 +2,13 @@
 // import './App.css';
 import Header from "./components/Header";
 import Card from "./components/Card";
+import React from "react";
 
 export default function App() {
     //set current score
+    const [currentScore, setCurrentScore] = React.useState(0);
     //set highest/best score
+    const [highScore, setHighScore] = React.useState(0);
     //render cards on load and re-render on each click in randomised order
 
     return (
@@ -13,6 +16,10 @@ export default function App() {
             <Header />
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <main>
+                <div className="scoreboard">
+                    <p>Current score : {currentScore}</p>
+                    <p>High score : {highScore}</p>
+                </div>
                 <div className="card-placement">
                     <Card />
                 </div>
