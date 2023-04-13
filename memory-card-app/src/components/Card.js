f1e21320a3deaf7e69b1b28ecb2d5bd15fe0a2a2
Stefan <stefanseifeddine@gmail.com>
2023-04-13 22:05:05 +0100

    Create Card component

---
+++ memory-card-app/src/components/Card.js
@@ -0,0 +1,9 @@
+export default function Card() {
+    //set card state here
+    return (
+        <div>
+            {/*render card*/}
+            <p>render card here</p>
+        </div>
+    );
+}
