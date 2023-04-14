cd02080e36abfa9fc4975401b57df5f861b67a62
Stefan <stefanseifeddine@gmail.com>
2023-04-14 21:56:49 +0100

    Add array of flag objects to access flag images

---
+++ memory-card-app/src/images/flags.js
@@ -0,0 +1,29 @@
+import flagArg from "./01.png";
+import flagGre from "./02.png";
+import flagUru from "./03.png";
+import flagFin from "./04.png";
+
+const flags = [
+    {
+        id: 0,
+        flag: flagArg,
+    },
+    {
+        id: 1,
+        flag: flagGre,
+    },
+    {
+        id: 2,
+        flag: flagUru,
+    },
+    {
+        id: 3,
+        flag: flagFin,
+    },
+];
+
+const flagsArray = () => {
+    return flags;
+};
+
+export default flagsArray();
