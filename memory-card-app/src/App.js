7f1cd57fc3f506fbe77edea2a3cf28ea9d0fc3d9
Stefan <stefanseifeddine@gmail.com>
2023-04-14 22:01:48 +0100

    Import images array and manually render some cards

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -1,8 +1,11 @@
 // import logo from "./logo.svg";
-// import './App.css';
+import "./styles/App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import React from "react";
+
+// import flagArg from "./images/01.png";
+import allImages from "./images/flags";
 
 export default function App() {
     //set current score
@@ -10,6 +13,12 @@ export default function App() {
     //set highest/best score
     const [highScore, setHighScore] = React.useState(0);
     //render cards on load and re-render on each click in randomised order
+    const [allFlags, setAllFlags] = React.useState(allImages);
+
+    console.log(allImages);
+    // console.log(allFlags);
+    console.table(allFlags);
+    // console.log(allImages[0].flag);
 
     return (
         <div className="App">
@@ -21,7 +30,12 @@ export default function App() {
                     <p>High score : {highScore}</p>
                 </div>
                 <div className="card-placement">
-                    <Card />
+                    <Card imageSource={allImages[0].flag} />
+                    <Card imageSource={allImages[1].flag} />
+                    <Card imageSource={allImages[2].flag} />
+                    <Card imageSource={allFlags[3].flag} />
+                    {/* <Card imageSource={flagArg} /> */}
+                    {/* <Card imageSource="https://images.dog.ceo/breeds/whippet/n02091134_14363.jpg" /> */}
                 </div>
             </main>
         </div>
