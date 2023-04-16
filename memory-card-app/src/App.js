e4ea079695acdf7d232ef7595de4c171b54fdc21
Stefan <stefanseifeddine@gmail.com>
2023-04-16 18:49:10 +0100

    Map card elements from importedFlagsArr onto page

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -4,38 +4,49 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 import React from "react";
 
-// import flagArg from "./images/01.png";
-import allImages from "./images/flags";
+// import array of country objects
+import importedFlagsArray from "./images/flags";
 
 export default function App() {
     //set current score
     const [currentScore, setCurrentScore] = React.useState(0);
+
     //set highest/best score
     const [highScore, setHighScore] = React.useState(0);
-    //render cards on load and re-render on each click in randomised order
-    const [allFlags, setAllFlags] = React.useState(allImages);
 
-    console.log(allImages);
-    // console.log(allFlags);
-    console.table(allFlags);
-    // console.log(allImages[0].flag);
+    //render cards on load and re-render on each click in randomised order
+    const [initialRenderArray, setInitialRenderArray] =
+        React.useState(importedFlagsArray);
+
+    console.table(initialRenderArray);
+
+    const cardElements = initialRenderArray.map(
+        (country) => (
+            <Card
+                imageSource={country.flag}
+                key={country.id}
+                // handleClick={handleClick}
+                value={country.wasClicked}
+                clicked={country.id}
+            />
+        )
+        // console.log(country.flag)
+    );
 
     return (
         <div className="App">
+            {console.log("%c--------render app-------", "background: yellow")}
             <Header />
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <main>
                 <div className="scoreboard">
                     <p>Current score : {currentScore}</p>
                     <p>High score : {highScore}</p>
                 </div>
                 <div className="card-placement">
-                    <Card imageSource={allImages[0].flag} />
-                    <Card imageSource={allImages[1].flag} />
-                    <Card imageSource={allImages[2].flag} />
-                    <Card imageSource={allFlags[3].flag} />
                     {/* <Card imageSource={flagArg} /> */}
                     {/* <Card imageSource="https://images.dog.ceo/breeds/whippet/n02091134_14363.jpg" /> */}
+                    {cardElements}
+                    {/* {renderFlags()} */}
                 </div>
             </main>
         </div>
