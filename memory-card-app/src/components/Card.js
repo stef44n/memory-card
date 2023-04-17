9ff3b5bb5b44238b882ac542ea5b0f76f040ca5b
Stefan <stefanseifeddine@gmail.com>
2023-04-17 01:53:42 +0100

    Add clickedState and display it on the card

--- memory-card-app/src/components/Card.js
+++ memory-card-app/src/components/Card.js
@@ -1,11 +1,38 @@
+import React from "react";
 import "../styles/Card.css";
 
 export default function Card(props) {
     //set card state here
+    const [clickedState, setClickedState] = React.useState(false);
+
+    function handleCardClick() {
+        // setClickedState(true);
+        setClickedState((prevState) => !prevState);
+    }
+
     return (
-        <div className="card">
+        <div
+            className="card"
+            value={`${clickedState}`}
+            onClick={props.handleClick}
+        >
             {/*render card*/}
-            <img className="card-image" src={props.imageSource} alt="card" />
+            <h1>
+                {props.clicked} {`${props.clicked}`}
+            </h1>
+            <p>{`${clickedState}`}</p>
+            <img
+                className="card-image"
+                src={props.imageSource}
+                alt="card"
+                // data-value={`${clickedState}`}
+                onClick={handleCardClick}
+                // defaultValue={props.clicked}
+                // value={clicked}
+                // defVal={clickedState}
+                // onClick={props.handleClick}
+                // value={props.value}
+            />
         </div>
     );
 }
