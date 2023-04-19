d2b7a310453e39d4cdcd5879c4694525a61455a4
Stefan <stefanseifeddine@gmail.com>
2023-04-20 00:33:45 +0100

    Add conditional class name

--- memory-card-app/src/components/Card.js
+++ memory-card-app/src/components/Card.js
@@ -5,33 +5,58 @@ export default function Card(props) {
     //set card state here
     const [clickedState, setClickedState] = React.useState(false);
 
-    function handleCardClick() {
+    // let resetAll = setClickedState(props.reSet);
+    // let resetAll = props.reSet ? setClickedState(true) : false;
+    // console.log(`%c PROPS.RESET: ${resetAll}`, "background: lightgrey");
+
+    function handleCardClick(bool) {
         // setClickedState(true);
-        setClickedState((prevState) => !prevState);
+        bool = clickedState;
+        let thisBool = bool;
+        // setClickedState(false);
+        // if (thisBool === "false") {
+        //     setClickedState(true);
+        //     console.log(`thisBool 1: ${bool}`);
+        // } else {
+        //     setClickedState(false);
+        //     console.log(`thisBool 2: ${bool}`);
+        // }
+        console.log(`%c props.reSet: ${props.reSet}`, "background: orange");
+        console.log(`%c bool: ${bool}`, "background: orange");
+        console.log(`clickedState: ${clickedState}`);
+        if (thisBool === true) {
+            console.log(`thisBool 1: ${bool}`);
+            // setClickedState(false);
+        } else if (thisBool === false) {
+            console.log(`thisBool 2: ${bool}`);
+            setClickedState(true);
+        } else if (thisBool === "true") {
+            console.log(`thisBool 3: ${bool}`);
+        } else if (thisBool === "false") {
+            console.log(`thisBool 4: ${bool}`);
+        }
     }
 
     return (
         <div
-            className="card"
+            // className="card"
+            className={`card ${clickedState}`}
             value={`${clickedState}`}
             onClick={props.handleClick}
         >
             {/*render card*/}
-            <h1>
-                {props.clicked} {`${props.clicked}`}
-            </h1>
-            <p>{`${clickedState}`}</p>
+            <h1>{`${clickedState}`}</h1>
+            <p>
+                {props.clicked}
+                {/* {`${props.clicked}`} */}
+            </p>
             <img
                 className="card-image"
                 src={props.imageSource}
                 alt="card"
                 // data-value={`${clickedState}`}
-                onClick={handleCardClick}
-                // defaultValue={props.clicked}
-                // value={clicked}
-                // defVal={clickedState}
-                // onClick={props.handleClick}
-                // value={props.value}
+                onClick={() => handleCardClick(props.reSet)}
+                // onClick={() => handleCardClick(false)}
             />
         </div>
     );
