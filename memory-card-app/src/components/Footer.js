f0ba50da706df4d6bcbf5bd9c601cf387f51ff7f
Stefan <stefanseifeddine@gmail.com>
2023-04-21 22:33:39 +0100

    Rewrite as functional component

--- memory-card-app/src/components/Footer.js
+++ memory-card-app/src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles/Footer.css";
 
-export default class Footer extends React.Component {
-    render() {
-        return (
-            <footer>
-                <h6>Copyright © 2023 Stef44n</h6>
-            </footer>
-        );
-    }
+export default function Footer() {
+    return (
+        <footer>
+            <h6>Copyright © 2023 Stef44n</h6>
+        </footer>
+    );
 }
