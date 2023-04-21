9b16a8ff9c14d6c36c6d12eb88c4be155b59e639
Stefan <stefanseifeddine@gmail.com>
2023-04-21 20:05:41 +0100

    Add more flags

--- memory-card-app/src/images/flags.js
+++ memory-card-app/src/images/flags.js
@@ -2,6 +2,14 @@ import flagArg from "./01.png";
 import flagGre from "./02.png";
 import flagUru from "./03.png";
 import flagFin from "./04.png";
+import flagIce from "./05.png";
+import flagNor from "./06.png";
+import flagDen from "./07.png";
+import flagSwi from "./08.png";
+import flagVie from "./09.png";
+import flagMor from "./10.png";
+import flagMon from "./11.png";
+import flagAlb from "./12.png";
 
 const flags = [
     {
@@ -19,6 +27,38 @@ const flags = [
     {
         id: 3,
         flag: flagFin,
+    },
+    {
+        id: 4,
+        flag: flagIce,
+    },
+    {
+        id: 5,
+        flag: flagNor,
+    },
+    {
+        id: 6,
+        flag: flagDen,
+    },
+    {
+        id: 7,
+        flag: flagSwi,
+    },
+    {
+        id: 8,
+        flag: flagVie,
+    },
+    {
+        id: 9,
+        flag: flagMor,
+    },
+    {
+        id: 10,
+        flag: flagMon,
+    },
+    {
+        id: 11,
+        flag: flagAlb,
     },
 ];
 
