4f71f726432f31fa1ab037b5978c0f24b7e5c238
Stefan <stefanseifeddine@gmail.com>
2023-04-21 21:46:26 +0100

    Add footer

--- memory-card-app/src/App.js
+++ memory-card-app/src/App.js
@@ -1,5 +1,6 @@
 import "./styles/App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Card from "./components/Card";
 import React from "react";
 
@@ -196,6 +197,7 @@ export default function App() {
                     {/* {renderState && reRenderFlags()} */}
                 </div>
             </main>
+            <Footer />
         </div>
     );
 }
---
+++ memory-card-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <h6>Copyright © 2023 Stef44n</h6>
+            </footer>
+        );
+    }
+}
